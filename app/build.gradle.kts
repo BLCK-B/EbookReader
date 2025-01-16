54a339e6a7a471d6321b808286def71bd7282207
BLCK <123077751+BLCK-B@users.noreply.github.com>
2025-01-16 11:09:19 +0100

    package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.reader"
+    namespace = "com.blck.reader"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.reader"
+        applicationId = "com.blck.reader"
         minSdk = 26
         targetSdk = 34
         versionCode = 2
--- app/src/test/java/com/example/reader/ExampleUnitTest.java
+++ app/src/test/java/com/example/reader/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.example.reader;
+package com.blck.reader;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
